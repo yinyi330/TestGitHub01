483d06fa7f2271a9d2eacb899b776c7003895f59
yinyi330 <yy18683783118@163.com>
2018-03-08 11:41:56 +0800

    测试提交！

--- src/com/yy/test/Test.java
+++ src/com/yy/test/Test.java
@@ -5,4 +5,8 @@ package com.yy.test;
  */
 public class Test {
         private  int  id =1111;
+
+        public static void main(String[] args) {
+                System.out.println("测试提交！");
+        }
 }
